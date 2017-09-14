0ae20b531e496703537d75def32751f4103427d3
Robert Roos <roosen7@users.noreply.github.com>
2017-09-14 10:46:45 +0300

    Lisatud on esimene rida php koodi

---
+++ esimene.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html> 
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>
+	Robert Roosi leht
+	</title>
+</head>
+<body>
+	<h1>
+	Robert Roos, veebiprogrammeerimine
+	</h1>
+	<p>
+	See veebileht on loodud õppetöö raames ning ei sisalda mingisugust tõsiseltvõetavat sisu!
+	</p>
+		<?php
+			echo "Algas PHP õppimine.";
+		?>
+</body>
+</html>
